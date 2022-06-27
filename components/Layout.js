86d7b4fad5cb1936de411878a0f9646a53a1e259
Kalen Willits <KalenWillits@gmail.com>
2022-06-27 13:50:16 -0400

    Fixing margins and word break

--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,9 @@ import Footer from "../components/Footer";
 
 const Layout = ({children, activeTab}) => {
   return (
-    <div className="overflow-x-hidden py-12 w-screen h-screen bg-zinc-900">
+    <div className="overflow-x-hidden py-12 w-screen h-screen bg-zinc-900 break-words">
       <Header activeTab={activeTab}/>
-      <div className="px-14 pt-4">
+      <div className="px-4 md:px-16 pt-4">
       {children}
     </div>
     <div className="my-10">
