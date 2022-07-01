e72e2b3d063488d6aed968a842a874001f145af5
Kalen Willits <KalenWillits@gmail.com>
2022-07-01 15:32:49 -0400

    Adding 2 logs

--- data/logs.js
+++ data/logs.js
@@ -2,6 +2,29 @@ export const logs =
   [
     {
       title:
+        "Lexicons, the websocket server framework",
+      timestamp:
+        "July 1st, 2022",
+      body: `
+I have uploaded the new Python package Lexicons to github and made it available on PyPI. This performant
+database and websocket server framework has been a long-term project of mine to fuel writing multiplayer online
+games. At the time of this writing, Lexicons has successfully integrated with Pygame and Godot to create
+multiplayer online experiences.
+  `,
+    },
+
+    {
+      title:
+        "nbk Public Release",
+      timestamp:
+        "July 1st, 2022",
+      body: `
+I have released a version 1.0.0 prototype of my project "nbk", a terminal application for keeping developers on track
+without sacrifcing time or screen space. In this release I added an export to csv funcionality.
+  `,
+    },
+    {
+      title:
         "My portfolio is live!",
       timestamp:
         "June 27th, 2022",
