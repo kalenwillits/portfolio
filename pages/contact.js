7d59d453a782d464201ef09c0ef9bcbf0e7cbf0a
Kalen <kalenwillits@gmail.com>
2023-09-03 21:12:10 -0400

    Typo fix on contact page.

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     <Layout activeTab="Contact">
       <div className="grid grid-cols-1 items-center p-4 text-gray-300 rounded-lg border md:mt-10">
         <p className="pb-4 border-b">
-          Looking to get in touch? Send me an email or connect on LikedIn.
+          Looking to get in touch? Send me an email or connect on LinkedIn.
         </p>
       <div className="mt-4">
         LinkedIn: <a href="https://www.linkedin.com/in/kalenwillits/" className="text-sky-500" target="_blank" >https://www.linkedin.com/in/kalenwillits/</a>
